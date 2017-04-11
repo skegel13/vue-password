139d21df91a0d8aa887089d367c5b5d9e2a644f4
Sean Kegel <skegel13@gmail.com>
2017-04-11 17:42:20 -0400

    Added placeholder attriute to input

--- src/components/VuePasswordCustom.vue
+++ src/components/VuePasswordCustom.vue
@@ -3,6 +3,7 @@
     <div class="VuePassword__Input">
       <input type="password"
            :name="name"
+           :placeholder="placeholder"
            :id="id"
            :class="classes"
            :value="value"
@@ -174,6 +175,14 @@
       name: {
         type: String,
         default: 'password'
+      },
+
+      /**
+       * Placeholder attribute of the password input.
+       */
+      placeholder: {
+        type: [Boolean, String],
+        default: false
       },
 
       /**
